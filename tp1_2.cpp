b37f33be2ac9c11494d7fa1f12b310b6aaa04b32
Luciana Khalil <khalilmarialuciana@gmail.com>
2024-03-31 09:07:11 -0300

    ejercicio 2.4 completado

---
+++ tp1_2.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+    int Cuadrado(int num) {
+    return num * num;
+}
+
+void CuadradoVoid(int num) {
+    int resultado= num * num;
+    printf("El cuadrado de %d es %d\n", num, resultado);
+}
+
+void mostrarVariables(int variable){
+    printf("El contenido de la variable es: %d \n", variable);
+    printf("La direccion de la variable es: %p \n", &variable);
+
+}
+void Invertir(int a, int b) {
+    int aux=a;
+    a = b;
+    b = aux;
+    printf("Los valores invertidos son: %d %d\n", a, b);
+}
+
+// e) Función para devolver los valores ordenados
+void Orden(int a, int b) {
+  int aux;
+  if (a > b) {
+    aux = a;
+    a = b;
+    b = aux;
+     printf("Los valores ordenados son: %d %d\n", a, b);
+  }
+
+}
+
+int main(){
+     int numero,variable, x, y;
+    printf("Ingrese un numero entero de 1 a 10:\n");
+    scanf("%d", &numero);
+
+    printf("El cuadrado de %d es %d\n", numero, Cuadrado(numero));
+    Cuadrado(numero);
+    CuadradoVoid(numero);
+
+    printf("Ingrese una variable entera de 1 a 10:\n");
+    scanf("%d", &variable);
+
+    mostrarVariables(variable);
+
+    printf("Ingrese un numero x de 1 a 10:\n");
+    scanf("%d", &x);
+    printf("Ingrese un numero y de 1 a 10:\n");
+    scanf("%d", &y);
+
+    Invertir(x, y);
+    Orden(x, y);
+
+    printf("Los valores ordenados son: %d %d\n", x, y);
+    return 0;
+}
