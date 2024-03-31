b1dfd46d788bdada00a14983396251fccd518612
Luciana Khalil <khalilmarialuciana@gmail.com>
2024-03-31 02:17:49 -0300

    segundo commit

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hola Mundo");
+    int variable = 7;
+    int *puntero = &variable;// Declarar un puntero que apunte a la variable
+
+    // Mostrar el contenido del puntero
+    printf("1) Contenido del puntero: %d\n", *puntero);
+    printf("2) Dirección de memoria almacenada por el puntero: %p\n", puntero);
+    printf("3) Dirección de memoria de la variable: %p\n", &variable);
+    printf("4) Dirección de memoria del puntero: %p\n", &puntero);
+    printf("5) Tamaño de memoria utilizado por la variable: %zu bytes\n", sizeof(variable));
+
     return 0;
 }
